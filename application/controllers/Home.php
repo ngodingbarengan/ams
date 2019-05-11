adcf53b566dd58066f72dd1df92fafa288bae27e
ngodingbarengan <jualalkes99@gmail.com>
2019-05-11 16:33:46 +0700

    yoga1

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,7 +5,7 @@ class Home extends CI_Controller {
 
 	public function __construct()
 	{
-
+		// ...
 		parent::__construct();
 		$this->load->model('produk_model','produk');
 		$this->load->model('wishlist_model','wishlist');
